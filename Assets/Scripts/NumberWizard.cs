6b2422300dfaa5688dda2c963c9ec52eb84c6b87
eatorres <etorres_57@yahoo.com>
2017-02-04 23:32:19 -0500

    able to take basic input

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -8,10 +8,29 @@ public class NumberWizard : MonoBehaviour {
 	void Start () {
         print("Welcome to Number Wizard");
         print("Pick a number in your head and don't tell me:");
+        int low = 0;
+        int high = 1000;
+
+        print("The highest number you can pick is " + high);
+        print("The lowest number you can pick it" + low);
+
+        print("Is your number higher or lower than 500?");
+        print("Arrow up for higher, Arrow down for lower");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            print("Up arrow pressed");
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            print("Down arrow pressed");
+        }
+        else if (Input.GetKeyDown("Enter"))
+        {
+            print("I won!");
+        }
+    }
 }
