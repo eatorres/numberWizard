c36222628f8389df342ed5468907b4d4e781c532
Eduardo Alejandro Torres <eatorres@uwaterloo.ca>
2017-05-18 18:42:15 -0400

    finished UI, randomized guesses

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -1,56 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class NumberWizard : MonoBehaviour {
     int min;
     int max;
     int guess;
+    int maxGuessAllowed = 10;
+
+    public Text text;
+
     // Use this for initialization
     void Start () {
-        StarGame();
-	}
+        StartGame();
+        text.text = guess.ToString();
+    }
 
-	void StarGame()
+	void StartGame()
     {
         min = 1;
         max = 1001;
-        guess = 500;
-        print("Welcome to Number Wizard");
-        print("Pick a number in your head and don't tell me:");
-
-
-        print("The highest number you can pick is " + max);
-        print("The lowest number you can pick it" + min);
-
-        print("Is your number higher or lower than " + guess + "?");
-        print("Arrow up for higher, Arrow down for lower");
+        guess = Random.Range(1, 1001);
     }
 
     void NextGuess()
     {
-        guess = (min + max) / 2;
-        print("Higher or lower than " + guess);
+        guess = Random.Range(min, max + 1);
+        text.text = guess.ToString();
+        maxGuessAllowed -= 1;
+
+        if (maxGuessAllowed <= 0) {
+            SceneManager.LoadScene("Win");
+        }
     }
 
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            print("Up arrow pressed");
-            min = guess;
-            NextGuess();
-        }
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            print("Down arrow pressed");
-            max = guess;
-            NextGuess();
-        }
-        else if (Input.GetKeyDown(KeyCode.Return))
-        {
-            print("I won!");
-            StarGame();
-        }
+    public void GuessHigher() {
+        min = guess;
+        NextGuess();
+    }
+
+    public void GuessLower() {
+        max = guess;
+        NextGuess();
     }
 }
